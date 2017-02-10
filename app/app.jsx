b05ebcc7732baf8628ef43becb1b7d88fd2a527d
Linda Haviv <Linda.Haviv@foxnews.com>
2017-02-10 18:11:41 -0500

    added another component

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,13 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require{'react-router'}
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('./components/Main');
 
 ReactDOM.render(
   <Router history={hashHistory}>
-  	<Route path="/" component={}> 
+  	<Route path="/" component={Main}> 
+
   	</Route>
   </Router>, 
   document.getElementById('app')
