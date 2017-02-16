b0f42d0a1092012314e5229d561bcfdb647fc67f
LindaHaviv <linda.haviv@gmail.com>
2017-02-16 01:33:22 -0500

    style about page & link resources

--- public/bundle.js
+++ public/bundle.js
@@ -15054,17 +15054,46 @@ var React = __webpack_require__(4);
 
 var About = function About(props) {
   return React.createElement(
-    'div',
+    "div",
     null,
     React.createElement(
-      'h3',
-      null,
-      'About'
+      "h1",
+      { className: "text-center" },
+      "About"
     ),
     React.createElement(
-      'p',
+      "p",
       null,
-      'Welcome to the about page!'
+      "This is a weather application build on React. I have built this for The Complete React Web App Developer Course."
+    ),
+    React.createElement(
+      "p",
+      null,
+      "Here are some of the tools I used:"
+    ),
+    React.createElement(
+      "ul",
+      null,
+      React.createElement(
+        "li",
+        null,
+        React.createElement(
+          "a",
+          { href: "https://facebook.github.io/react" },
+          "React"
+        ),
+        " - This was the JavaScript framework used."
+      ),
+      React.createElement(
+        "li",
+        null,
+        React.createElement(
+          "a",
+          { href: "http://openweathermap.org" },
+          "Open Weather Map"
+        ),
+        " - I used Open Weather Map to search for weather data by city name."
+      )
     )
   );
 };
